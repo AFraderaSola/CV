1583a46525d8f562998ae84b1b03dc440be1dd73
AFraderaSola <66006724+AFraderaSola@users.noreply.github.com>
2021-04-30 12:55:16 +0200

    Add files via upload

---
+++ Script.R
@@ -0,0 +1,6 @@
+datadrivencv::use_datadriven_cv(
+  full_name = "Albert Fradera Sola",
+  data_location = "U:/Desktop/Cool_Packages_and_Rescources/CV/data/",
+  pdf_location = "https://github.com/AFraderaSola/CV/blob/master/cv.pdf",
+  html_location = "https://github.com/AFraderaSola/CV/blob/master/cv.html",
+  source_location = "https://github.com/AFraderaSola/CV")
